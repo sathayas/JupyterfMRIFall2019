aba02bf18f1a7517899a82a733e59e9415b83fe0
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-08 13:32:30 -0600

    added plot_svm_margin function

--- DecisionBoundary.py
+++ DecisionBoundary.py
@@ -39,3 +39,42 @@ def plot_contours(ax, clf, x, y, h=.02, **params):
     Z = Z.reshape(xx.shape)
     out = ax.contourf(xx, yy, Z, **params)
     return out
+
+
+def plot_svm_margin(X,y,sv):
+    '''
+    Input:
+         X:  2D data matrix
+         y:  Target vector
+         sv: Support vector machine results
+    Returns:
+         None
+    Produces:
+         A scatter plot of the data X, with colors defined by y.
+         SVM boundary, as well as the margin is plotted. Also
+         support vectors are indicated.
+
+         You need to run plt.show() after everything is done.
+    '''
+    # scatter plot first
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=50)
+
+    # suppor for the meshgrid
+    ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    # create grid to evaluate model
+    xx = np.linspace(xlim[0], xlim[1], 30)
+    yy = np.linspace(ylim[0], ylim[1], 30)
+    YY, XX = np.meshgrid(yy, xx)
+    xy = np.vstack([XX.ravel(), YY.ravel()]).T
+    Z = sv.decision_function(xy).reshape(XX.shape)
+
+    # plot decision boundary and margins
+    ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+
+    # plot support vectors
+    ax.scatter(sv.support_vectors_[:, 0], sv.support_vectors_[:, 1], s=200,
+               linewidth=1, edgecolors='k', facecolors='none')
